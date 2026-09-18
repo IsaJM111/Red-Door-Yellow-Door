2ac0f23d4d3dd50f59a4990b13e72175ee49fa52
Isadora M <isadora.marta@gmail.com>
2026-09-18 06:18:31 -0500

    add clock death timer

---
+++ Assets/Scripts/clockTimer.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ClockDeath : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        StartCoroutine(DelayFunction());
+    }
+
+    IEnumerator DelayFunction()
+    {
+        Debug.Log("Before delay");
+        yield return new WaitForSeconds(66); // Delay for 5 seconds
+        Debug.Log("After delay");
+        SceneManager.LoadSceneAsync("Credt Scene");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
